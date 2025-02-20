fe3b4cf4d5d7b4d071cff017dbad2fdfeb04e552
Hammad Liaquat <154601362+hammadliaquat77@users.noreply.github.com>
2025-02-20 17:18:30 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import './App.css'
-import Login from './Components/Login/Login'
-import Profile from './Components/Profile/Profile'
-import UserProvider from './Context/UserContextProvider'
 
 function App() {
 
   return (
     <>
-      <UserProvider>
-        <div className='flex justify-center items-center flex-col h-screen'>
-          <Login />
-          <Profile />
-        </div>
-      </UserProvider>
+      
     </>
   )
 }
